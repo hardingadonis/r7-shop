20cd4f05844e49bf9c8a907b3edbafd0f320c832
Minh Vương <hardingadonis@gmail.com>
2023-11-08 22:56:06 +0700

    Add DBContextMySQLImpl class, impl for DBContext class
    Signed-off-by: Minh Vương <hardingadonis@gmail.com>

--- src/main/java/io/hardingadonis/r7/utils/Singleton.java
+++ src/main/java/io/hardingadonis/r7/utils/Singleton.java
@@ -1,8 +1,13 @@
 package io.hardingadonis.r7.utils;
 
 import io.hardingadonis.r7.services.*;
+import io.hardingadonis.r7.services.impl.mysql.*;
 
 public class Singleton {
 
     public static DBContext context;
+
+    static {
+        context = new DBContextMySQLImpl();
+    }
 }
