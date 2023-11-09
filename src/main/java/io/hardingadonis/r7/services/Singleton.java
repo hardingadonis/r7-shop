960e2e7e6926ad9cbe87550ee76258eed4acd53c
Minh Vương <hardingadonis@gmail.com>
2023-11-09 07:33:23 +0700

    Move Singleton class to services package
    Signed-off-by: Minh Vương <hardingadonis@gmail.com>

--- src/main/java/io/hardingadonis/r7/services/Singleton.java
+++ src/main/java/io/hardingadonis/r7/services/Singleton.java
@@ -1,13 +1,18 @@
 package io.hardingadonis.r7.services;
 
-import io.hardingadonis.r7.services.*;
+import io.hardingadonis.r7.dao.*;
+import io.hardingadonis.r7.dao.impl.mysql.*;
 import io.hardingadonis.r7.services.impl.mysql.*;
 
 public class Singleton {
 
     public static DBContext context;
+    
+    public static AdminDAO adminDAO;
 
     static {
         context = new DBContextMySQLImpl();
+        
+        adminDAO = new AdminDAOMySQLImpl();
     }
 }
